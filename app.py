78c57d59730940746028234e99e45f7710ad4246
Srikanthreddy Maddireddy <maddireddysrikanth15@gmail.com>
2025-03-08 12:02:55 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ import pandas as pd
 def load_data():
     movies = pd.read_csv("movie.csv",low_memory= False)
     tags = pd.read_csv("tag.csv", low_memory= False)
-    #ratings = pd.read_csv(r"C:\Users\haris\Desktop\vs_code\IMDB_data_analysis\rating.csv", low_memory=False)
-    return movies, tags  #, ratings
+    return movies, tags
 
 # Load datasets
 movies, tags = load_data()
@@ -22,7 +21,6 @@ st.subheader("Select any checkbox below to display the records:")
 # Checkboxes to select which dataset to display
 show_movies = st.checkbox("Show Movies Dataset")
 show_tags = st.checkbox("Show Tags Dataset")
-#show_ratings = st.checkbox("Show Ratings Dataset")
 
 # Slider for selecting the number of records to display (Max 30)
 num_records = st.slider("Select number of records to display:", min_value=0,max_value=30)
@@ -35,7 +33,3 @@ if show_movies:
 if show_tags:
     st.subheader("Tags Dataset")
     st.dataframe(tags.head(num_records))
-
-#if show_ratings:
-    #st.subheader("Ratings Dataset")
-    #st.dataframe(ratings.head(num_records))
